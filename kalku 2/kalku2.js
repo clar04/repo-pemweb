b5d70e1d92e94225e5926dad989aa90a8bb09ae9
clar04 <123356941+clar04@users.noreply.github.com>
2023-09-19 12:15:56 +0700

    Add files via upload

---
+++ kalku 2/kalku2.js
@@ -0,0 +1,6 @@
+(function(){
+    let calculator = document.querySelector('.calculator');
+    let num = document.querySelector('.num');
+    let clear = document.querySelector('.clear');
+    let equal = document.querySelector('.equal');
+})();
